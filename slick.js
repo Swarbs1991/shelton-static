e59f5f4fdaec7a56254ddb6651d467d4b5744e6b
Joe Swarbrick <jswarb1991@outlook.com>
2025-07-29 15:30:03 +0100

    Sliders added with Slick Slider support

---
+++ slick.js
@@ -0,0 +1,44 @@
+$(function(){
+    // Testimonial slider - default mode
+    $('.testimonial-slider').slick({
+        dots: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        adaptiveHeight: true
+    });
+
+    // Songs slider with center mode, no arrows
+    $('.songs-slider').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        arrows: false, // <-- arrows removed
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 1,
+                    centerPadding: '20px'
+                }
+            }
+        ]
+    });
+
+    // Packages slider - multiple items, dots, no arrows
+    $('.packages-slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+});
