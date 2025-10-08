5092826b7f44d074c084aa6c207f3eae4290625e
Joe Swarbrick <jswarb1991@outlook.com>
2025-10-08 21:55:01 +0100

    Added gallery and tried implementing reCAPTCHA on form

--- slick.js
+++ slick.js
@@ -1,4 +1,22 @@
 $(function(){
+// Gallery slider
+    $('.gallery-slider').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        arrows: false, // <-- arrows removed
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 1,
+                    centerPadding: '20px'
+                }
+            }
+        ]
+    });
+
     // Testimonial slider - default mode
     $('.testimonial-slider').slick({
         dots: true,
@@ -41,4 +59,5 @@ $(function(){
             }
         ]
     });
+
 });
