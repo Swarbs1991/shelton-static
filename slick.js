7303212c7e36f88e8f8f4119c092fb656ce2187a
Joe Swarbrick <jswarb1991@outlook.com>
2025-10-22 18:44:59 +0100

    Mobile fettling 430px and under

--- slick.js
+++ slick.js
@@ -1,63 +1,78 @@
 $(function(){
-// Gallery slider
-    $('.gallery-slider').slick({
-        centerMode: true,
-        centerPadding: '60px',
-        slidesToShow: 3,
-        arrows: false, // <-- arrows removed
-        dots: true,
-        responsive: [
-            {
-                breakpoint: 900,
-                settings: {
-                    slidesToShow: 1,
-                    centerPadding: '20px'
-                }
-            }
-        ]
-    });
+  // Gallery slider
+      $('.gallery-slider').slick({
+          centerMode: true,
+          centerPadding: '60px',
+          slidesToShow: 3,
+          arrows: false,
+          dots: true,
+          responsive: [
+              {
+                  breakpoint: 900,
+                  settings: {
+                      slidesToShow: 1,
+                      centerPadding: '20px'
+                  }
+              },
+              {
+                  breakpoint: 430,
+                  settings: {
+                      slidesToShow: 1,
+                      centerPadding: '20px',
+                      dots: true
+                  }
+              }
+          ]
+      });
 
-    // Testimonial slider - default mode
-    $('.testimonial-slider').slick({
-        dots: true,
-        arrows: false,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        adaptiveHeight: true
-    });
+      // Testimonial slider - default mode
+      $('.testimonial-slider').slick({
+          dots: true,
+          arrows: false,
+          autoplay: true,
+          autoplaySpeed: 5000,
+          adaptiveHeight: true
+      });
 
-    // Songs slider with center mode, no arrows
-    $('.songs-slider').slick({
-        centerMode: true,
-        centerPadding: '60px',
-        slidesToShow: 3,
-        arrows: false, // <-- arrows removed
-        dots: true,
-        responsive: [
-            {
-                breakpoint: 900,
-                settings: {
-                    slidesToShow: 1,
-                    centerPadding: '20px'
-                }
-            }
-        ]
-    });
+      // Songs slider with center mode, no arrows
+      $('.songs-slider').slick({
+          centerMode: true,
+          centerPadding: '60px',
+          slidesToShow: 3,
+          arrows: false,
+          dots: true,
+          responsive: [
+              {
+                  breakpoint: 900,
+                  settings: {
+                      slidesToShow: 1,
+                      centerPadding: '20px'
+                  }
+              }
+          ]
+      });
 
-    // Packages slider - multiple items, dots, no arrows
-    $('.packages-slider').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        dots: false,
-        arrows: false,
-        responsive: [
-            {
-                breakpoint: 900,
-                settings: {
-                    slidesToShow: 1
-                }
-            }
-        ]
-    });
+      // Packages slider - multiple items, dots only on small screens
+      $('.packages-slider').slick({
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          dots: false,
+          arrows: false,
+          responsive: [
+              {
+                  breakpoint: 900,
+                  settings: {
+                      slidesToShow: 1
+                  }
+              },
+              {
+                  breakpoint: 430,
+                  settings: {
+                      slidesToShow: 1,
+                      dots: true
+                  }
+              }
+          ]
+      });
 
 });
